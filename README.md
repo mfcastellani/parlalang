184466429005d31c622ce91bb1b6c912232be698
Marcelo Castellani <marcelo@linux.com>
2020-01-14 10:19:43 -0300

    Improved readme

--- README.md
+++ README.md
@@ -1,38 +1,52 @@
-parlalang
-=========
+# parlalang
 
-Parla programming language, for learning purpouses
+## About
+
+*Parla programming language*, for learning purpouses
 
 Parla is a small programming language created for learning purpouses, just for fun.
+
+Parla means *speaks" in italian. The name was choosen because you have methodos to show details about variables and understand how the memory is at the moment.
 
 Currently it just work with integer numbers, stored in a structure. 
 
 You can store data into the structure using load method:
 
+```
 load 10 into a
+```
 
 or
 
+```
 load a * b into c
+```
 
 You can show the value using say
 
+```
 say a
+```
 
 and show the structure using say details about or just about
 
+```
 say details about a
 about a
+```
 
 You can use basic flow control with if and while
 
+```
 while (a <= 10) do
 	say a
 	load a + 1 into a
 end
+```
 
 or 
 
+```
 if (d >= e) do
 	say d
 	about d
@@ -40,9 +54,15 @@ end else do
 	say e
 	about e
 end
+```
 
 So you can use Parla to explain logic to your students or just for fun, or as an base to your own programming language.
 
 I hope this can be useful to you and if you agree just let me know.
 
 Cheers!
+
+## Buildind
+
+You need build-essentials and bison. Take a look at `makefile`.
+
--- y.tab.h
+++ y.tab.h
@@ -1,14 +1,13 @@
-/* A Bison parser, made by GNU Bison 2.3.  */
+/* A Bison parser, made by GNU Bison 3.0.4.  */
 
-/* Skeleton interface for Bison's Yacc-like parsers in C
+/* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
-   Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
 
-   This program is free software; you can redistribute it and/or modify
+   This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,9 +15,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.  */
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -33,37 +30,46 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-/* Tokens.  */
+#ifndef YY_YY_Y_TAB_H_INCLUDED
+# define YY_YY_Y_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     INTEGER = 258,
-     FLOAT = 259,
-     VARIABLE = 260,
-     WHILE = 261,
-     IF = 262,
-     PRINT = 263,
-     EOL = 264,
-     INTO = 265,
-     LOADTOKEN = 266,
-     QUIT = 267,
-     INSPECT = 268,
-     DEFTOKEN = 269,
-     TYPETOKEN = 270,
-     BCHAR_TYPE = 271,
-     ECHAR_TYPE = 272,
-     NUMBER_TYPE = 273,
-     VERSION = 274,
-     IFX = 275,
-     ELSE = 276,
-     NE = 277,
-     EQ = 278,
-     LE = 279,
-     GE = 280,
-     UMINUS = 281
-   };
+  enum yytokentype
+  {
+    INTEGER = 258,
+    FLOAT = 259,
+    VARIABLE = 260,
+    WHILE = 261,
+    IF = 262,
+    PRINT = 263,
+    EOL = 264,
+    INTO = 265,
+    LOADTOKEN = 266,
+    QUIT = 267,
+    INSPECT = 268,
+    DEFTOKEN = 269,
+    TYPETOKEN = 270,
+    BCHAR_TYPE = 271,
+    ECHAR_TYPE = 272,
+    NUMBER_TYPE = 273,
+    VERSION = 274,
+    IFX = 275,
+    ELSE = 276,
+    GE = 277,
+    LE = 278,
+    EQ = 279,
+    NE = 280,
+    UMINUS = 281
+  };
 #endif
 /* Tokens.  */
 #define INTEGER 258
@@ -85,30 +91,34 @@
 #define VERSION 274
 #define IFX 275
 #define ELSE 276
-#define NE 277
-#define EQ 278
-#define LE 279
-#define GE 280
+#define GE 277
+#define LE 278
+#define EQ 279
+#define NE 280
 #define UMINUS 281
 
-
-
-
+/* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE
-#line 20 "parla.y"
+
+union YYSTYPE
 {
+#line 20 "parla.y" /* yacc.c:1909  */
+
     int iValue;                 /* integer value */
     char sIndex;                /* symbol table index */
     nodeType *nPtr;             /* node pointer */
-}
-/* Line 1529 of yacc.c.  */
-#line 107 "y.tab.h"
-	YYSTYPE;
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
-# define YYSTYPE_IS_DECLARED 1
+
+#line 112 "y.tab.h" /* yacc.c:1909  */
+};
+
+typedef union YYSTYPE YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
 #endif
+
 
 extern YYSTYPE yylval;
 
+int yyparse (void);
+
+#endif /* !YY_YY_Y_TAB_H_INCLUDED  */
